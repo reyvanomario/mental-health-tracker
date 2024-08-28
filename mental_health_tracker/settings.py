7d5283020129f0d6d7fc26d72369dee23f45e361
reyvanomario <reyvanomario24122004@gmail.com>
2024-08-28 15:48:59 +0700

    menambahkan URL deployment PWS pada ALLOWED_HOSTS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n85*q!2qnng^4afav^xt2a(_rurz_@$pd+$2f)-k#3ptfwmy(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "reyvano-mario-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
