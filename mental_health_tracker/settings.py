4d18e1a7956d81c86397e6b8b73e768fa881de95
reyvanomario <reyvanomario24122004@gmail.com>
2024-09-04 15:26:53 +0700

    Menyelesaikan tutorial 1

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "reyvano-mario-mentalhealthtracker.pb
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
