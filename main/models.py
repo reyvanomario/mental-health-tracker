183d5572c9788feba26ba80e37a9a5bc4e0aef95
reyvanomario <reyvanomario24122004@gmail.com>
2024-09-11 12:50:20 +0700

    testing

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ class MoodEntry(models.Model):
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
 
+
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
