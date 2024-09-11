8c4c8878093f95e1358be2790b6f9abbc21c76e7
reyvanomario <reyvanomario24122004@gmail.com>
2024-09-11 15:45:12 +0700

    add ci/cd PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://reyvano-mario-mentalhealthtracker.pbp.cs.ui.ac.id", "https://reyvano-mario-mentalhealthtracker.pbp.cs.ui.ac.id"]
