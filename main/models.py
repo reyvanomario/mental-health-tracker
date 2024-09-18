8fd323fd0873aeba4a89578798b285d504f4b46c
reyvanomario <reyvanomario24122004@gmail.com>
2024-09-18 15:39:30 +0700

    menyelesaikan tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,9 +1,13 @@
 import uuid
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class MoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
@@ -14,3 +18,4 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+    
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n85*q!2qnng^4afav^xt2a(_rurz_@$pd+$2f)-k#3ptfwmy(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "reyvano-mario-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
